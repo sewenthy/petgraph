c2d59bf48b3898c28623bba417f0cc6a4c43f84f
Sewen Thy <me@sewenthy.dev>
2023-03-31 18:13:32 +0800

    ext4-expr-active

--- src/dot.rs
+++ src/dot.rs
@@ -1,52 +1,49 @@
-//! Simple graphviz dot file format output.
-
-use std::fmt::{self, Display, Formatter, Write};
-
+#![doc = " Simple graphviz dot file format output."]
 use crate::visit::{
     EdgeRef, GraphProp, IntoEdgeReferences, IntoNodeReferences, NodeIndexable, NodeRef,
 };
-
-/// `Dot` implements output to graphviz .dot format for a graph.
-///
-/// Formatting and options are rather simple, this is mostly intended
-/// for debugging. Exact output may change.
-///
-/// # Examples
-///
-/// ```
-/// use petgraph::Graph;
-/// use petgraph::dot::{Dot, Config};
-///
-/// let mut graph = Graph::<_, ()>::new();
-/// graph.add_node("A");
-/// graph.add_node("B");
-/// graph.add_node("C");
-/// graph.add_node("D");
-/// graph.extend_with_edges(&[
-///     (0, 1), (0, 2), (0, 3),
-///     (1, 2), (1, 3),
-///     (2, 3),
-/// ]);
-///
-/// println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
-///
-/// // In this case the output looks like this:
-/// //
-/// // digraph {
-/// //     0 [label="\"A\""]
-/// //     1 [label="\"B\""]
-/// //     2 [label="\"C\""]
-/// //     3 [label="\"D\""]
-/// //     0 -> 1
-/// //     0 -> 2
-/// //     0 -> 3
-/// //     1 -> 2
-/// //     1 -> 3
-/// //     2 -> 3
-/// // }
-///
-/// // If you need multiple config options, just list them all in the slice.
-/// ```
+use std::fmt::{self, Display, Formatter, Write};
+#[doc = " `Dot` implements output to graphviz .dot format for a graph."]
+#[doc = ""]
+#[doc = " Formatting and options are rather simple, this is mostly intended"]
+#[doc = " for debugging. Exact output may change."]
+#[doc = ""]
+#[doc = " # Examples"]
+#[doc = ""]
+#[doc = " ```"]
+#[doc = " use petgraph::Graph;"]
+#[doc = " use petgraph::dot::{Dot, Config};"]
+#[doc = ""]
+#[doc = " let mut graph = Graph::<_, ()>::new();"]
+#[doc = " graph.add_node(\"A\");"]
+#[doc = " graph.add_node(\"B\");"]
+#[doc = " graph.add_node(\"C\");"]
+#[doc = " graph.add_node(\"D\");"]
+#[doc = " graph.extend_with_edges(&["]
+#[doc = "     (0, 1), (0, 2), (0, 3),"]
+#[doc = "     (1, 2), (1, 3),"]
+#[doc = "     (2, 3),"]
+#[doc = " ]);"]
+#[doc = ""]
+#[doc = " println!(\"{:?}\", Dot::with_config(&graph, &[Config::EdgeNoLabel]));"]
+#[doc = ""]
+#[doc = " // In this case the output looks like this:"]
+#[doc = " //"]
+#[doc = " // digraph {"]
+#[doc = " //     0 [label=\"\\\"A\\\"\"]"]
+#[doc = " //     1 [label=\"\\\"B\\\"\"]"]
+#[doc = " //     2 [label=\"\\\"C\\\"\"]"]
+#[doc = " //     3 [label=\"\\\"D\\\"\"]"]
+#[doc = " //     0 -> 1"]
+#[doc = " //     0 -> 2"]
+#[doc = " //     0 -> 3"]
+#[doc = " //     1 -> 2"]
+#[doc = " //     1 -> 3"]
+#[doc = " //     2 -> 3"]
+#[doc = " // }"]
+#[doc = ""]
+#[doc = " // If you need multiple config options, just list them all in the slice."]
+#[doc = " ```"]
 pub struct Dot<'a, G>
 where
     G: IntoEdgeReferences + IntoNodeReferences,
@@ -56,27 +53,23 @@ where
     get_node_attributes: &'a dyn Fn(G, G::NodeRef) -> String,
     config: Configs,
 }
-
 static TYPE: [&str; 2] = ["graph", "digraph"];
 static EDGE: [&str; 2] = ["--", "->"];
 static INDENT: &str = "    ";
-
 impl<'a, G> Dot<'a, G>
 where
     G: IntoNodeReferences + IntoEdgeReferences,
 {
-    /// Create a `Dot` formatting wrapper with default configuration.
+    #[doc = " Create a `Dot` formatting wrapper with default configuration."]
     #[inline]
     pub fn new(graph: G) -> Self {
         Self::with_config(graph, &[])
     }
-
-    /// Create a `Dot` formatting wrapper with custom configuration.
+    #[doc = " Create a `Dot` formatting wrapper with custom configuration."]
     #[inline]
     pub fn with_config(graph: G, config: &'a [Config]) -> Self {
         Self::with_attr_getters(graph, config, &|_, _| String::new(), &|_, _| String::new())
     }
-
     #[inline]
     pub fn with_attr_getters(
         graph: G,
@@ -93,49 +86,25 @@ where
         }
     }
 }
-
-/// `Dot` configuration.
-///
-/// This enum does not have an exhaustive definition (will be expanded)
-// TODO: #[non_exhaustive] once MSRV >= 1.40,
-// and/or for a breaking change make this something like an EnumSet: https://docs.rs/enumset
+#[doc = " `Dot` configuration."]
+#[doc = ""]
+#[doc = " This enum does not have an exhaustive definition (will be expanded)"]
 #[derive(Debug, PartialEq, Eq)]
 pub enum Config {
-    /// Use indices for node labels.
+    #[doc = " Use indices for node labels."]
     NodeIndexLabel,
-    /// Use indices for edge labels.
+    #[doc = " Use indices for edge labels."]
     EdgeIndexLabel,
-    /// Use no edge labels.
+    #[doc = " Use no edge labels."]
     EdgeNoLabel,
-    /// Use no node labels.
+    #[doc = " Use no node labels."]
     NodeNoLabel,
-    /// Do not print the graph/digraph string.
+    #[doc = " Do not print the graph/digraph string."]
     GraphContentOnly,
     #[doc(hidden)]
     _Incomplete(()),
 }
-macro_rules! make_config_struct {
-    ($($variant:ident,)*) => {
-        #[allow(non_snake_case)]
-        #[derive(Default)]
-        struct Configs {
-            $($variant: bool,)*
-        }
-        impl Configs {
-            #[inline]
-            fn extract(configs: &[Config]) -> Self {
-                let mut conf = Self::default();
-                for c in configs {
-                    match *c {
-                        $(Config::$variant => conf.$variant = true,)*
-                        Config::_Incomplete(()) => {}
-                    }
-                }
-                conf
-            }
-        }
-    }
-}
+macro_rules ! make_config_struct { ($ ($ variant : ident ,) *) => { # [allow (non_snake_case)] # [derive (Default)] struct Configs { $ ($ variant : bool ,) * } impl Configs { # [inline] fn extract (configs : & [Config]) -> Self { let mut conf = Self :: default () ; for c in configs { match * c { $ (Config ::$ variant => conf .$ variant = true ,) * Config :: _Incomplete (()) => { } } } conf } } } }
 make_config_struct!(
     NodeIndexLabel,
     EdgeIndexLabel,
@@ -143,7 +112,6 @@ make_config_struct!(
     NodeNoLabel,
     GraphContentOnly,
 );
-
 impl<'a, G> Dot<'a, G>
 where
     G: IntoNodeReferences + IntoEdgeReferences + NodeIndexable + GraphProp,
@@ -157,8 +125,6 @@ where
         if !self.config.GraphContentOnly {
             writeln!(f, "{} {{", TYPE[g.is_directed() as usize])?;
         }
-
-        // output all labels
         for node in g.node_references() {
             write!(f, "{}{} [ ", INDENT, g.to_index(node.id()),)?;
             if !self.config.NodeNoLabel {
@@ -172,16 +138,16 @@ where
             }
             writeln!(f, "{}]", (self.get_node_attributes)(g, node))?;
         }
-        // output all edges
-        self.bar____EXTRACT_THIS(f, edge_fmt, g);
-
+        self.bar(f, &edge_fmt, g);
         if !self.config.GraphContentOnly {
             writeln!(f, "}}")?;
         }
         Ok(())
     }
-
-    fn bar____EXTRACT_THIS<EF>(&self, f: &mut Formatter<'a>, edge_fmt: EF, g: G) where EF: Fn(&G::EdgeWeight, &mut fmt::Formatter) -> fmt::Result {
+    fn bar<'lt0, 'lt1, 'lt2, EF>(&self, f: &'lt0 mut Formatter<'lt1>, edge_fmt: &'lt2 EF, g: G)
+    where
+        EF: Fn(&G::EdgeWeight, &mut fmt::Formatter) -> fmt::Result,
+    {
         for (i, edge) in g.edge_references().enumerate() {
             write!(
                 f,
@@ -190,13 +156,14 @@ where
                 g.to_index(edge.source()),
                 EDGE[g.is_directed() as usize],
                 g.to_index(edge.target()),
-            ).unwrap();
+            )
+            .unwrap();
             if !self.config.EdgeNoLabel {
                 write!(f, "label = \"").unwrap();
                 if self.config.EdgeIndexLabel {
                     write!(f, "{}", i).unwrap();
                 } else {
-                    Escaped(FnFmt(edge.weight(), &edge_fmt)).fmt(f).unwrap();
+                    Escaped(FnFmt(edge.weight(), &(*edge_fmt))).fmt(f).unwrap();
                 }
                 write!(f, "\" ").unwrap();
             }
@@ -204,7 +171,6 @@ where
         }
     }
 }
-
 impl<'a, G> fmt::Display for Dot<'a, G>
 where
     G: IntoEdgeReferences + IntoNodeReferences + NodeIndexable + GraphProp,
@@ -215,7 +181,6 @@ where
         self.graph_fmt(f, fmt::Display::fmt, fmt::Display::fmt)
     }
 }
-
 impl<'a, G> fmt::Debug for Dot<'a, G>
 where
     G: IntoEdgeReferences + IntoNodeReferences + NodeIndexable + GraphProp,
@@ -226,10 +191,8 @@ where
         self.graph_fmt(f, fmt::Debug::fmt, fmt::Debug::fmt)
     }
 }
-
-/// Escape for Graphviz
+#[doc = " Escape for Graphviz"]
 struct Escaper<W>(W);
-
 impl<W> fmt::Write for Escaper<W>
 where
     W: fmt::Write,
@@ -240,21 +203,17 @@ where
         }
         Ok(())
     }
-
     fn write_char(&mut self, c: char) -> fmt::Result {
         match c {
             '"' | '\\' => self.0.write_char('\\')?,
-            // \l is for left justified linebreak
             '\n' => return self.0.write_str("\\l"),
             _ => {}
         }
         self.0.write_char(c)
     }
 }
-
-/// Pass Display formatting through a simple escaping filter
+#[doc = " Pass Display formatting through a simple escaping filter"]
 struct Escaped<T>(T);
-
 impl<T> fmt::Display for Escaped<T>
 where
     T: fmt::Display,
@@ -267,10 +226,8 @@ where
         }
     }
 }
-
-/// Format data using a specific format function
+#[doc = " Format data using a specific format function"]
 struct FnFmt<'a, T, F>(&'a T, F);
-
 impl<'a, T, F> fmt::Display for FnFmt<'a, T, F>
 where
     F: Fn(&'a T, &mut fmt::Formatter<'_>) -> fmt::Result,
@@ -279,14 +236,12 @@ where
         self.1(self.0, f)
     }
 }
-
 #[cfg(test)]
 mod test {
     use super::{Config, Dot, Escaper};
     use crate::prelude::Graph;
     use crate::visit::NodeRef;
     use std::fmt::Write;
-
     #[test]
     fn test_escape() {
         let mut buff = String::new();
@@ -296,7 +251,6 @@ mod test {
         }
         assert_eq!(buff, "\\\" \\\\ \\l");
     }
-
     fn simple_graph() -> Graph<&'static str, &'static str> {
         let mut graph = Graph::<&str, &str>::new();
         let a = graph.add_node("A");
@@ -304,28 +258,24 @@ mod test {
         graph.add_edge(a, b, "edge_label");
         graph
     }
-
     #[test]
     fn test_nodeindexlable_option() {
         let graph = simple_graph();
         let dot = format!("{:?}", Dot::with_config(&graph, &[Config::NodeIndexLabel]));
-        assert_eq!(dot, "digraph {\n    0 [ label = \"0\" ]\n    1 [ label = \"1\" ]\n    0 -> 1 [ label = \"\\\"edge_label\\\"\" ]\n}\n");
+        assert_eq ! (dot , "digraph {\n    0 [ label = \"0\" ]\n    1 [ label = \"1\" ]\n    0 -> 1 [ label = \"\\\"edge_label\\\"\" ]\n}\n");
     }
-
     #[test]
     fn test_edgeindexlable_option() {
         let graph = simple_graph();
         let dot = format!("{:?}", Dot::with_config(&graph, &[Config::EdgeIndexLabel]));
-        assert_eq!(dot, "digraph {\n    0 [ label = \"\\\"A\\\"\" ]\n    1 [ label = \"\\\"B\\\"\" ]\n    0 -> 1 [ label = \"0\" ]\n}\n");
+        assert_eq ! (dot , "digraph {\n    0 [ label = \"\\\"A\\\"\" ]\n    1 [ label = \"\\\"B\\\"\" ]\n    0 -> 1 [ label = \"0\" ]\n}\n");
     }
-
     #[test]
     fn test_edgenolable_option() {
         let graph = simple_graph();
         let dot = format!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
-        assert_eq!(dot, "digraph {\n    0 [ label = \"\\\"A\\\"\" ]\n    1 [ label = \"\\\"B\\\"\" ]\n    0 -> 1 [ ]\n}\n");
+        assert_eq ! (dot , "digraph {\n    0 [ label = \"\\\"A\\\"\" ]\n    1 [ label = \"\\\"B\\\"\" ]\n    0 -> 1 [ ]\n}\n");
     }
-
     #[test]
     fn test_nodenolable_option() {
         let graph = simple_graph();
@@ -335,7 +285,6 @@ mod test {
             "digraph {\n    0 [ ]\n    1 [ ]\n    0 -> 1 [ label = \"\\\"edge_label\\\"\" ]\n}\n"
         );
     }
-
     #[test]
     fn test_with_attr_getters() {
         let graph = simple_graph();
@@ -348,6 +297,6 @@ mod test {
                 &|_, nr| format!("label = \"{}\"", nr.weight().to_lowercase()),
             ),
         );
-        assert_eq!(dot, "digraph {\n    0 [ label = \"a\"]\n    1 [ label = \"b\"]\n    0 -> 1 [ label = \"EDGE_LABEL\"]\n}\n");
+        assert_eq ! (dot , "digraph {\n    0 [ label = \"a\"]\n    1 [ label = \"b\"]\n    0 -> 1 [ label = \"EDGE_LABEL\"]\n}\n");
     }
 }
