3d14f2184cc46b4de08314841b5820dc2c1674ef
Sewen Thy <me@sewenthy.dev>
2023-03-31 16:56:58 +0800

    failed

--- src/graphmap.rs
+++ src/graphmap.rs
@@ -663,15 +663,7 @@ where
         if Ty::is_directed() {
             let self_dir = self.dir;
             let start_node = self.start_node;
-            (&mut self.iter)
-                .filter_map(move |&(n, dir)| {
-                    if dir == self_dir || n == start_node {
-                        Some(n)
-                    } else {
-                        None
-                    }
-                })
-                .next()
+            self.fun_name(self_dir, start_node)
         } else {
             self.iter.next().map(|&(n, _)| n)
         }
@@ -683,6 +675,24 @@ where
         } else {
             (lower, upper)
         }
+    }
+}
+
+impl<'a, N, Ty> NeighborsDirected<'a, N, Ty>
+where
+    N: 'a,
+    Ty: EdgeType,
+{
+    fn fun_name<N>(&mut self, self_dir: Direction, start_node: N) -> Option<N> where N: NodeTrait {
+        (&mut self.iter)
+            .filter_map(move |&(n, dir)| {
+                if dir == self_dir || n == start_node {
+                    Some(n)
+                } else {
+                    None
+                }
+            })
+            .next()
     }
 }
 
