ba5cadb48742c953d2c98562b5ccaf3adfb124cf
Sewen Thy <me@sewenthy.dev>
2023-03-31 18:13:02 +0800

    ext1-expr-active

--- src/generate.rs
+++ src/generate.rs
@@ -1,43 +1,30 @@
-//! ***Unstable.*** Graph generation.
-//!
-//! ***Unstable: API may change at any time.*** Depends on `feature = "generate"`.
-//!
-
+#![doc = " ***Unstable.*** Graph generation."]
+#![doc = ""]
+#![doc = " ***Unstable: API may change at any time.*** Depends on `feature = \"generate\"`."]
+#![doc = ""]
 use crate::graph::NodeIndex;
 use crate::{Directed, EdgeType, Graph};
-
-// A DAG has the property that the adjacency matrix is lower triangular,
-// diagonal zero.
-//
-// This means we only allow edges i → j where i < j.
-//
-// The set of all DAG of a particular size is simply the power set of all
-// possible edges.
-//
-// For a graph of n=3 nodes we have (n - 1) * n / 2 = 3 possible edges.
-
-/// A graph generator of “all” graphs of a particular size.
-///
-/// ***Unstable: API may change at any time.*** Depends on `feature = "generate"`.
+#[doc = " A graph generator of “all” graphs of a particular size."]
+#[doc = ""]
+#[doc = " ***Unstable: API may change at any time.*** Depends on `feature = \"generate\"`."]
 pub struct Generator<Ty> {
     acyclic: bool,
     selfloops: bool,
     nodes: usize,
-    /// number of possible edges
+    #[doc = " number of possible edges"]
     nedges: usize,
-    /// current edge bitmap
+    #[doc = " current edge bitmap"]
     bits: u64,
     g: Graph<(), (), Ty>,
 }
-
 impl Generator<Directed> {
-    /// Generate all possible Directed acyclic graphs (DAGs) of a particular number of vertices.
-    ///
-    /// These are only generated with one per isomorphism, so they use
-    /// one canonical node labeling where node *i* can only have edges to node *j* if *i < j*.
-    ///
-    /// For a graph of *k* vertices there are *e = (k - 1) k / 2* possible edges and
-    /// *2<sup>e</sup>* DAGs.
+    #[doc = " Generate all possible Directed acyclic graphs (DAGs) of a particular number of vertices."]
+    #[doc = ""]
+    #[doc = " These are only generated with one per isomorphism, so they use"]
+    #[doc = " one canonical node labeling where node *i* can only have edges to node *j* if *i < j*."]
+    #[doc = ""]
+    #[doc = " For a graph of *k* vertices there are *e = (k - 1) k / 2* possible edges and"]
+    #[doc = " *2<sup>e</sup>* DAGs."]
     pub fn directed_acyclic(nodes: usize) -> Self {
         assert!(nodes != 0);
         let nedges = (nodes - 1) * nodes / 2;
@@ -52,16 +39,15 @@ impl Generator<Directed> {
         }
     }
 }
-
 impl<Ty: EdgeType> Generator<Ty> {
-    /// Generate all possible graphs of a particular number of vertices.
-    ///
-    /// All permutations are generated, so the graphs are not unique down to isomorphism.
-    ///
-    /// For a graph of *k* vertices there are *e = k²* possible edges and
-    /// *2<sup>k<sup>2</sup></sup>* graphs.
+    #[doc = " Generate all possible graphs of a particular number of vertices."]
+    #[doc = ""]
+    #[doc = " All permutations are generated, so the graphs are not unique down to isomorphism."]
+    #[doc = ""]
+    #[doc = " For a graph of *k* vertices there are *e = k²* possible edges and"]
+    #[doc = " *2<sup>k<sup>2</sup></sup>* graphs."]
     pub fn all(nodes: usize, allow_selfloops: bool) -> Self {
-        let mut nedges = Self::bar____EXTRACT_THIS(nodes, allow_selfloops);
+        let mut nedges = Self::bar(nodes, allow_selfloops);
         Generator {
             acyclic: false,
             selfloops: allow_selfloops,
@@ -71,8 +57,7 @@ impl<Ty: EdgeType> Generator<Ty> {
             g: Graph::with_capacity(nodes, nedges),
         }
     }
-
-    fn bar____EXTRACT_THIS(nodes: usize, allow_selfloops: bool) -> usize {
+    fn bar(nodes: usize, allow_selfloops: bool) -> usize {
         let scale = if Ty::is_directed() { 1 } else { 2 };
         let nedges = if allow_selfloops {
             (nodes * nodes - nodes) / scale + nodes
@@ -82,19 +67,11 @@ impl<Ty: EdgeType> Generator<Ty> {
         assert!(nedges < 64);
         nedges
     }
-
     fn state_to_graph(&mut self) -> &Graph<(), (), Ty> {
         self.g.clear();
         for _ in 0..self.nodes {
             self.g.add_node(());
         }
-        // For a DAG:
-        // interpret the bits in order, it's a lower triangular matrix:
-        //   a b c d
-        // a x x x x
-        // b 0 x x x
-        // c 1 2 x x
-        // d 3 4 5 x
         let mut bit = 0;
         for i in 0..self.nodes {
             let start = if self.acyclic || !self.g.is_directed() {
@@ -109,13 +86,11 @@ impl<Ty: EdgeType> Generator<Ty> {
                 if self.bits & (1u64 << bit) != 0 {
                     self.g.add_edge(NodeIndex::new(i), NodeIndex::new(j), ());
                 }
-
                 bit += 1;
             }
         }
         &self.g
     }
-
     pub fn next_ref(&mut self) -> Option<&Graph<(), (), Ty>> {
         if self.bits == !0 {
             self.bits = 0;
@@ -128,10 +103,8 @@ impl<Ty: EdgeType> Generator<Ty> {
         Some(self.state_to_graph())
     }
 }
-
 impl<Ty: EdgeType> Iterator for Generator<Ty> {
     type Item = Graph<(), (), Ty>;
-
     fn next(&mut self) -> Option<Self::Item> {
         self.next_ref().cloned()
     }
